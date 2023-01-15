a093ed4a64bbc17d360f3d00e9c60ffa0d4de969
odairdias <odair2d@hotmail.com>
2023-01-15 18:56:24 -0300

    trabalhando loop projeto

--- proj_ant_ref.py
+++ proj_ant_ref.py
@@ -4,48 +4,54 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time 
 
-lista_compras=[]
-
-
-principal='https://lista.mercadolivre.com.br/'
 
 busca=input('Qual será a pesquisa de hoje?: ')
+
+for url_completa in range(1,301,51):
+
+    lista_compras=[]
+
+    principal='https://lista.mercadolivre.com.br/'
+
+    #paginas=f'_Desde_{ml}_NoIndex_True'
+
+  
 #print(principal+busca)
+    url_completa= (principal+busca+'_Desde_{url_completa}_NoIndex_True')
 
+    resposta= requests.get(url_completa)
 
-resposta= requests.get(principal+busca)
-time.sleep(6)
-ml=BeautifulSoup(resposta.text, 'html.parser')
+    ml=BeautifulSoup(resposta.text, 'html.parser')
 
-produtos=  ml.findAll( 'div', attrs= {'class':'andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core ui-search-result--advertisement andes-card--padding-default
-'})
+    produtos=  ml.findAll( 'div', attrs= {'class':'"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"'})
+
 
 # ui-search-result__content-wrapper shops__result-content-wrapper
 #"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"
 
-for produto in produtos:
+    for produto in produtos:
     
-    nome_produto=produto.find('h2', attrs={'class':'ui-search-item__title'})
+        nome_produto=produto.find('h2', attrs={'class':'ui-search-item__title'})
 
-    link_produto=produto.find('a', attrs={'class':'ui-search-link'})
+        link_produto=produto.find('a', attrs={'class':'ui-search-link'})
     
-    preço_produtoreal= produto.find('span', attrs={'class':'price-tag-fraction'})
+        preço_produtoreal= produto.find('span', attrs={'class':'price-tag-fraction'})
     
-    preço_produtocentavos= produto.find('span', attrs={'class':'price-tag-cents'})
+        preço_produtocentavos= produto.find('span', attrs={'class':'price-tag-cents'})
     
-    if (preço_produtocentavos):
-        preçototal=(preço_produtoreal.text  + ',' + preço_produtocentavos.text)
-    else:
-        preçototal=(preço_produtoreal.text) 
+        if (preço_produtocentavos):
+            preçototal=(preço_produtoreal.text  + ',' + preço_produtocentavos.text)
+        else:
+            preçototal=(preço_produtoreal.text) 
 
 
-    if(preço_produtocentavos):
+        if(preço_produtocentavos):
 
     
-        lista_compras.append([nome_produto.text,preçototal,link_produto['href']])
-    else:
-        lista_compras.append([nome_produto.text,preço_produtoreal.text,link_produto['href']])
-time.sleep(1) 
+            lista_compras.append([nome_produto.text,preçototal,link_produto['href']])
+        else:
+            lista_compras.append([nome_produto.text,preço_produtoreal.text,link_produto['href']])
+    time.sleep(1) 
     
 # print(ml.ar prettify())
     #print("Nome do Produto: ",nome_produto.text) 
