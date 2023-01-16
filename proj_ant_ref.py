2256bcc847ccdb7a75882ff60cfd28963f3319df
OdairDias <odair2d@hotmail.com>
2023-01-16 16:10:21 -0300

    ajustes
    Signed-off-by: OdairDias <odair2d@hotmail.com>

--- proj_ant_ref.py
+++ proj_ant_ref.py
@@ -7,30 +7,22 @@ import time
 
 busca=input('Qual será a pesquisa de hoje?: ')
 
-for url_completa in range(1,301,51):
+for limit in range(1, 152, 51):
 
     lista_compras=[]
 
     principal='https://lista.mercadolivre.com.br/'
 
-    #paginas=f'_Desde_{ml}_NoIndex_True'
+    #print(principal+busca)
 
-  
-#print(principal+busca)
-    url_completa= (principal+busca+'_Desde_{url_completa}_NoIndex_True')
-
-    resposta= requests.get(url_completa)
+    resposta= requests.get(principal+busca+'_Desde_{}_NoIndex_True'.format(limit) )
 
     ml=BeautifulSoup(resposta.text, 'html.parser')
 
-    produtos=  ml.findAll( 'div', attrs= {'class':'"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"'})
-
-
-# ui-search-result__content-wrapper shops__result-content-wrapper
-#"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"
+    produtos=  ml.find_all( 'div', attrs= {'class':"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"})
 
     for produto in produtos:
-    
+
         nome_produto=produto.find('h2', attrs={'class':'ui-search-item__title'})
 
         link_produto=produto.find('a', attrs={'class':'ui-search-link'})
@@ -42,17 +34,20 @@ for url_completa in range(1,301,51):
         if (preço_produtocentavos):
             preçototal=(preço_produtoreal.text  + ',' + preço_produtocentavos.text)
         else:
-            preçototal=(preço_produtoreal.text) 
+         preçototal=(preço_produtoreal.text) 
 
 
         if(preço_produtocentavos):
 
-    
             lista_compras.append([nome_produto.text,preçototal,link_produto['href']])
         else:
             lista_compras.append([nome_produto.text,preço_produtoreal.text,link_produto['href']])
-    time.sleep(1) 
+        time.sleep(1)
+
     
+
+    
+
 # print(ml.ar prettify())
     #print("Nome do Produto: ",nome_produto.text) 
     #print('link do produto:',link_produto['href'])
@@ -61,7 +56,6 @@ for url_completa in range(1,301,51):
     #else:
        #print('Preço do produto: R$',preço_produtoreal.text) 
     #print('\n\n')
-
 
 
 pesquisa=pd.DataFrame(lista_compras,columns=['Produto','Preço produto','Link produto'])
