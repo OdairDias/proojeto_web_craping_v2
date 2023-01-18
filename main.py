4129dee495896c0f24d6d39aae5726ef9541281e
OdairDias <odair2d@hotmail.com>
2023-01-18 14:45:04 -0300

    loop
    Signed-off-by: OdairDias <odair2d@hotmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import pandas as pd
-from bs4 import BeautifulSoup
+#bibliotecas 
 import requests
-import time
-import htmllib
-requirements.txt
+from bs4 import BeautifulSoup
+import pandas as pd
+import time 
+
 lista_compras=[]
 
 principal='https://lista.mercadolivre.com.br/'
@@ -12,29 +12,54 @@ busca=input('Qual será a pesquisa de hoje?: ')
 #print(principal+busca)
 
 
-ml=BeautifulSoup(resposta.text, 'html.parser')
+num_loop = int(input('Digite o número de Páginas: '))
 
+#aqui estamos limitando o número de paginas a no máximo 11 
+# caso queira mais paginas basta acrescentar a aprtir da 551 sempre de 50, a baixo a próxima seria por exemplo a 601 
 
-pagina = 1
-while True:
-    resposta = requests.get(principal+busca+'&page='+str(pagina))
-    ml = BeautifulSoup(resposta.text, 'html.parser')
-    produtos =  ml.findAll( 'div', attrs= {'class':"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"})
+pages_increment = ['0','51','101','151','201','251','301','351','401','501','551']
+
+for x in range(0,num_loop):
+
+    resposta= requests.get(principal+busca+'_Desde_'+pages_increment[x]+'_DisplayType_LF')
+
+    ml=BeautifulSoup(resposta.text, 'html.parser')
+
+    produtos=  ml.findAll( 'div', attrs= {'class':"andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"})
+
     for produto in produtos:
+    
         nome_produto=produto.find('h2', attrs={'class':'ui-search-item__title'})
+
         link_produto=produto.find('a', attrs={'class':'ui-search-link'})
+    
         preço_produtoreal= produto.find('span', attrs={'class':'price-tag-fraction'})
+    
         preço_produtocentavos= produto.find('span', attrs={'class':'price-tag-cents'})
+    
         if (preço_produtocentavos):
             preçototal=(preço_produtoreal.text  + ',' + preço_produtocentavos.text)
         else:
             preçototal=(preço_produtoreal.text) 
-        lista_compras.append([nome_produto.text,preçototal,link_produto['href']])
-    proxima_pagina = ml.find('a', attrs={'class':'andes-pagination__link andes-pagination__link--next'})
-    if proxima_pagina:
-        pagina += 1
-    else:
-        break
-    time.sleep(3)
+
+
+        if(preço_produtocentavos):
+
+            lista_compras.append([nome_produto.text,preçototal,link_produto['href']])
+        else:
+            lista_compras.append([nome_produto.text,preço_produtoreal.text,link_produto['href']])
+        time.sleep(1)
+# print(ml.ar prettify())
+    #print("Nome do Produto: ",nome_produto.text) 
+    #print('link do produto:',link_produto['href'])
+    #if (preço_produtocentavos):
+        #print('Preço do produto: R$',preço_produtoreal.text  + ',' + preço_produtocentavos.text)
+    #else:
+       #print('Preço do produto: R$',preço_produtoreal.text) 
+    #print('\n\n')
+
+
 pesquisa=pd.DataFrame(lista_compras,columns=['Produto','Preço produto','Link produto'])
-pesquisa.to_excel('resultado_pesquisa.xlsx', index=False)
+print(pesquisa)
+
+#pesquisa.to_excel('resultado_pesquisa.xlsx', index=False)
